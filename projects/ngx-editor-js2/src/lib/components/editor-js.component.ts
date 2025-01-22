75dd4d387d8fd1d94d9269a8142e01b0d5d2289f
Wesley DuSell <dusellfam@gmail.com>
2025-01-21 22:05:27 -0500

    Create simple POC of the Dyamaic Reactive Form

--- projects/ngx-editor-js2/src/lib/components/editor-js.component.ts
+++ projects/ngx-editor-js2/src/lib/components/editor-js.component.ts
@@ -1,12 +1,44 @@
-import { Component } from "@angular/core";
+import { Component, inject, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 
 @Component({
-  selector: "editor-js",
+  selector: 'editor-js',
+  imports: [MatButton, ReactiveFormsModule],
   template: `
-    <p>ngx-editor-js works!</p>
+    <button mat-button (click)="addFormControlComponent()">
+      Add Form Control Component
+    </button>
+    <form [formGroup]="formGroup">
+      <ng-container #ngxEditor></ng-container>
+    </form>
   `,
-  styles: []
+  styles: [],
 })
 export class EditorJsComponent {
-  
+  @ViewChild('ngxEditor', { read: ViewContainerRef, static: true })
+  ngxEditor!: ViewContainerRef;
+
+  formBuilder = inject(FormBuilder);
+  formGroup = this.formBuilder.group({});
+
+  addFormControlComponent() {
+    this.formGroup.addControl('test', this.formBuilder.control(''));
+    this.ngxEditor.createComponent(HeaderBlockComponent, { index: 0 });
+  }
+}
+
+@Component({
+  selector: 'header-block',
+  template: ` <h1 contentEditable>Header Block</h1> `,
+})
+export class HeaderBlockComponent implements ControlValueAccessor {
+  writeValue(_obj: any): void {}
+  registerOnChange(_fn: any): void {}
+  registerOnTouched(_fn: any): void {}
+  setDisabledState?(_isDisabled: boolean): void {}
 }
