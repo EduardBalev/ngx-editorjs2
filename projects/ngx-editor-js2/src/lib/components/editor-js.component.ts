4fd8857f6899d629a6b51a5effcb3c12166c12a4
Wesley DuSell <dusellfam@gmail.com>
2025-01-29 23:21:21 -0500

    idiomatically start the lib

--- projects/ngx-editor-js2/src/lib/components/editor-js.component.ts
+++ projects/ngx-editor-js2/src/lib/components/editor-js.component.ts
@@ -6,25 +6,21 @@ import {
   viewChild,
   ViewContainerRef,
 } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import {
   EditorJsService,
   NgxEditorJsBlock,
 } from '../services/editor-js.service';
 import { NgxEditorJs2Service } from '../services/ngx-editor-js2.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ToolFabService } from '../services/tool-fab.service';
 
 @Component({
   selector: 'editor-js',
-  imports: [ReactiveFormsModule],
   template: ` <ng-container #ngxEditor></ng-container> `,
 })
 export class EditorJsComponent {
-  toolFabService = inject(ToolFabService);
   editorJsService = inject(EditorJsService);
   ngxEditorJs2Service = inject(NgxEditorJs2Service);
-
+  
+  bootstrapEditorJs = input();
   blocks = input.required<NgxEditorJsBlock[]>();
   ngxEditor = viewChild.required('ngxEditor', { read: ViewContainerRef });
 
@@ -40,10 +36,6 @@ export class EditorJsComponent {
   // }
 
   constructor() {
-    this.ngxEditorJs2Service.loadBlocks$.pipe(takeUntilDestroyed()).subscribe();
-    this.toolFabService.toolbarComponentRef$
-      .pipe(takeUntilDestroyed())
-      .subscribe(); // 1st and only subscription?
     effect(() => {
       this.editorJsService.setNgxEditor(this.ngxEditor());
       this.ngxEditorJs2Service.blocksToLoad.next(this.blocks());
--- projects/ngx-editor-js2/src/lib/ngx-editor-js2.component.ts
+++ projects/ngx-editor-js2/src/lib/ngx-editor-js2.component.ts
@@ -1,14 +1,26 @@
-import { Component,input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { EditorJsComponent } from './components/editor-js.component';
 import { NgxEditorJsBlock } from './services/editor-js.service';
+import { AsyncPipe } from '@angular/common';
+import { combineLatest } from 'rxjs';
+import { ToolFabService } from './services/tool-fab.service';
+import { NgxEditorJs2Service } from './services/ngx-editor-js2.service';
 
 @Component({
   selector: 'ngx-editor-js2',
-  imports: [EditorJsComponent],
+  imports: [EditorJsComponent, AsyncPipe],
   template: `
-      <editor-js [blocks]="blocks()"></editor-js>
+    <editor-js
+      [blocks]="blocks()"
+      [bootstrapEditorJs]="bootstrapEditorJs$ | async"
+    ></editor-js>
   `,
 })
 export class NgxEditorJs2Component {
   blocks = input<NgxEditorJsBlock[]>([]);
+
+  bootstrapEditorJs$ = combineLatest([
+    inject(NgxEditorJs2Service).loadBlocks$,
+    inject(ToolFabService).toolbarComponentRef$,
+  ]);
 }
